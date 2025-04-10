93f7ce9fff47c97c200949f57e407f0e23a24b10
BrunoEijiMine <brunomine151@gmail.com>
2025-04-10 11:51:28 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div class="flex justify-center justify-items-center items-center h-screen">
+      <h1 class="bg-amber-950 text-amber-300 text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+
+  )
+}
+
+export default App
