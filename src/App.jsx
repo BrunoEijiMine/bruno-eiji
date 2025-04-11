cf536524c5c79216ef5e4788ef6193a1e5de05cd
BrunoEijiMine <brunomine151@gmail.com>
2025-04-11 11:11:10 -0300

    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import "./App.css";
+import "../src/ui/App.css";
+
+// import background from "";
 
 export default function App() {
   return (
     <div className="bg-500 font-[--font-poppins] h-screen flex items-center justify-center">
-      <div className="grid grid-cols-8 grid-rows-9 gap-4 w-[1440px]">
+
+      <div className="grid grid-cols-8 grid-rows-9 gap-4 w-[90rem]">
+
         <div className="grid col-span-2 row-start-1 row-span-3 col-start-1 grid-cols-1">
           <div className="bg-red-300 p-2">ASSINATURA</div>
         </div>
@@ -21,7 +25,9 @@ export default function App() {
         </div>
 
         <div className="grid col-span-4 row-start-9 row-span-1 col-start-3 grid-cols-1">
-          <div className="bg-black p-3">5</div>
+          <div className="bg-amber-100 p-3">
+            <div>p</div>
+          </div>
         </div>
 
         <div className="grid col-span-2 row-start-1 row-span-4 col-start-7 grid-cols-1">
